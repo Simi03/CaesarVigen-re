5b618ed88a68839a1decffdbaca0d7df73d25362
Simi03 <57719083+Simi03@users.noreply.github.com>
2021-02-25 11:52:55 +0100

    CaesarWithoutLimit

---
+++ src/main/java/Caesar.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * @author Simon Tobler
+ * @version 25/02/2021
+ */
+public class Caesar {
+    public static void CaesarVerschluesseln(){
+        Scanner sc = new  Scanner(System.in);
+        System.out.println("gebe den zu verschlüsselden text ein");
+        String inputText = sc.nextLine();
+        StringBuilder outputText = new StringBuilder();
+
+        System.out.println("Gebe die Anzahl ein um die Buchstaben zu verschlüsseln");
+        String verschiebungsZahl = sc.nextLine();
+
+        //Verschiebung
+        int x = Integer.parseInt(verschiebungsZahl); // Limit implementieren
+        for(int i=0; i< inputText.length();i++){
+            char zeichen = inputText.charAt(i);
+            if(zeichen != ' '){
+                outputText.append((char) (zeichen + x));
+            }else{
+                outputText.append((char) (' '));
+            }
+        }
+        System.out.println(outputText);
+
+    }
+    public static void main(String[] args) {
+        CaesarVerschluesseln();
+    }
+}
