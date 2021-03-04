116d0afcfcdb70812b492fa7978fc0792b9245fa
Simi03 <simon.tobler@stud.kbw.ch>
2021-03-04 11:50:08 +0100

    WorkingCaesarWithSmallLetters

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
  * @version 25/02/2021
  */
 public class Caesar {
-    public static void CaesarVerschluesseln(){
-        Scanner sc = new  Scanner(System.in);
+    public static void CaesarVerschluesseln() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("gebe den zu verschlüsselden text ein");
         String inputText = sc.nextLine();
         StringBuilder outputText = new StringBuilder();
@@ -16,17 +16,23 @@ public class Caesar {
 
         //Verschiebung
         int x = Integer.parseInt(verschiebungsZahl); // Limit implementieren
-        for(int i=0; i< inputText.length();i++){
+        for (int i = 0; i < inputText.length(); i++) {
             char zeichen = inputText.charAt(i);
-            if(zeichen != ' '){
-                outputText.append((char) (zeichen + x));
-            }else{
+            if (zeichen != ' ') {
+                if (zeichen + x > 122) {
+                    int rest = zeichen + x - 122; // 122 = z
+                    outputText.append((char) (96 + rest)); // 96 = 1 vor a
+                } else {
+                    outputText.append((char) (zeichen + x));
+                }
+            } else {
                 outputText.append((char) (' '));
             }
         }
         System.out.println(outputText);
 
     }
+
     public static void main(String[] args) {
         CaesarVerschluesseln();
     }
