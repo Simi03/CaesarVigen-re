ce7b909c6b4bf563e539eb858d8b75918869c13e
Simi03 <simon.tobler@stud.kbw.ch>
2021-03-11 11:57:47 +0100

    WorkingCaesarWithBigLetters

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
-
+// https://www.macronom.de/kryptographie/caesar/verschluesselung.php testing
 /**
  * @author Simon Tobler
  * @version 25/02/2021
  */
 public class Caesar {
+
     //Verschlüsselung
     public static void CaesarVerschluesseln() {
         Scanner sc = new Scanner(System.in);
@@ -20,11 +21,21 @@ public class Caesar {
         for (int i = 0; i < inputText.length(); i++) {
             char zeichen = inputText.charAt(i);
             if (zeichen != ' ') {
-                if (zeichen + x > 122) {
-                    int rest = zeichen + x - 122; // 122 = z
-                    outputText.append((char) (96 + rest)); // 96 = 1 vor a
-                } else {
-                    outputText.append((char) (zeichen + x));
+                // überprüft ob grossbuchstabe
+                if(Character.isUpperCase(zeichen)){
+                    if (zeichen + x > 90) {
+                        int rest = zeichen + x - 90; // 90 = Z
+                        outputText.append((char) (64 + rest)); // 64 = 1 vor A
+                    } else {
+                        outputText.append((char) (zeichen + x));
+                    }
+                }else { // falls es ein klein Buchstaben ist
+                    if (zeichen + x > 122) {
+                        int rest = zeichen + x - 122; // 122 = z
+                        outputText.append((char) (96 + rest)); // 96 = 1 vor a
+                    } else {
+                        outputText.append((char) (zeichen + x));
+                    }
                 }
             } else {
                 outputText.append((char) (' '));
@@ -45,15 +56,26 @@ public class Caesar {
         String verschiebungsZahl = sc.nextLine();
 
         //Verschiebung
+        // x ist die Verschiebungslänge (x=3 d.h. a--> d)
         int x = Integer.parseInt(verschiebungsZahl);
         for (int i = 0; i < inputText.length(); i++) {
             char zeichen = inputText.charAt(i);
             if (zeichen != ' ') {
-                if (zeichen - x < 97) {
-                    int rest = x + 97 - zeichen; // 97 = a
-                    outputText.append((char) (123 - rest)); // 123 = 1 nach z
-                } else {
-                    outputText.append((char) (zeichen - x));
+
+                if(Character.isUpperCase(zeichen)){ // falls Grossbuchstabe
+                    if (zeichen - x < 65) {
+                        int rest = x + 65 - zeichen; // 65 = A
+                        outputText.append((char) (91 - rest)); // 91 = 1 nach Z
+                    } else {
+                        outputText.append((char) (zeichen - x));
+                    }
+                }else{ //falls kleinbuchstabe
+                    if (zeichen - x < 97) {
+                        int rest = x + 97 - zeichen; // 97 = a
+                        outputText.append((char) (123 - rest)); // 123 = 1 nach z
+                    } else {
+                        outputText.append((char) (zeichen - x));
+                    }
                 }
             } else {
                 outputText.append((char) (' '));
@@ -64,7 +86,8 @@ public class Caesar {
     }
 
     public static void main(String[] args) {
+        // Balkendiagramm mit relativer häufigkeit erstellen
         //CaesarVerschluesseln();
-        //CaesarEntschluesseln();
+        CaesarEntschluesseln();
     }
 }
