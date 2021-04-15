c5544107058a755c6e5160c1d1487b3de5ab0af1
Simi03 <57719083+Simi03@users.noreply.github.com>
2021-04-15 12:08:36 +0200

    WorkingVigenere

--- src/main/java/Vigenere.java
+++ src/main/java/Vigenere.java
@@ -4,12 +4,36 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
-/**
- * @author Simon Lindemann
- * @version 08/04/2021
- */
 public class Vigenere {
+
+    public static char[] crypt(char[] plain, char[] key, int direction) {
+
+        char[] output = new char[plain.length];
+        for (int i = 0; i < plain.length; i++) {
+            //Verschluesseln
+            if (direction == 1) {
+                //Umrechnen für ASCII Tabelle
+                int result = (plain[i] + key[i % key.length]-194) % 26 ;
+                output[i] = (char) (result+97);
+            }
+            //Entschluesseln
+            else if (direction == 0){
+                int result;
+                if (plain[i] - key[i % key.length] < 0)
+                {
+                    result = (plain[i]- key[i % key.length]+130) %26 + 97;
+                }
+                else
+                {
+                    result = (plain[i] - key[i % key.length]) % 26 +97;
+                }
+                output[i] = (char) result;
+            }
+        }
+        return output;
+    }
     public static void createWriteFileEncrypt(String outputText) {
         // File erstellen
         try {
@@ -33,6 +57,7 @@ public class Vigenere {
         }
 
     }
+
     public static void createWriteFileDecrypt(String outputText) {
         // File erstellen
         try {
@@ -56,77 +81,26 @@ public class Vigenere {
         }
     }
 
-    // This function generates the key in
-    // a cyclic manner until it's length isi'nt
-    // equal to the length of original text
-    static String generateKey(String str, String key)
-    {
-        int x = str.length();
-
-        for (int i = 0; ; i++)
-        {
-            if (x == i)
-                i = 0;
-            if (key.length() == str.length())
-                break;
-            key+=(key.charAt(i));
-        }
-        return key;
-    }
-
-    // This function returns the encrypted text
-// generated with the help of the key
-    static String cipherText(String str, String key)
-    {
-        String cipher_text="";
-
-        for (int i = 0; i < str.length(); i++)
-        {
-            // converting in range 0-25
-            int x = (str.charAt(i) + key.charAt(i)) %26;
-
-            // convert into alphabets(ASCII)
-            x += 'A';
-
-            cipher_text+=(char)(x);
-        }
-        return cipher_text;
-    }
-
-    // This function decrypts the encrypted text
-// and returns the original text
-    static String originalText(String cipher_text, String key)
-    {
-        String orig_text="";
-
-        for (int i = 0 ; i < cipher_text.length() &&
-                i < key.length(); i++)
-        {
-            // converting in range 0-25
-            int x = (cipher_text.charAt(i) -
-                    key.charAt(i) + 26) %26;
-
-            // convert into alphabets(ASCII)
-            x += 'A';
-            orig_text+=(char)(x);
-        }
-        return orig_text;
-    }
-
     public static void main(String[] args) throws IOException {
-        String inputText = new String(Files.readAllBytes(Paths.get("src/main/resources/Klartext.txt")), StandardCharsets.UTF_8);
-        String keyword = "SHEEESH";
+        Scanner scanner = new Scanner(System.in);
+        String plaintext = new String(Files.readAllBytes(Paths.get("src/main/resources/Klartext.txt")), StandardCharsets.UTF_8);
+        System.out.println("Klartext: " + plaintext);
+        char[] plain = plaintext.toLowerCase().toCharArray();
 
-        String key = generateKey(inputText, keyword);
-        String cipher_text = (cipherText(inputText,key));
-        createWriteFileEncrypt(cipher_text);
-        createWriteFileDecrypt(originalText(cipher_text,key));
+        System.out.println("Schluesselwort eingeben:");
+        String keyword = scanner.nextLine();
+        keyword = keyword.toLowerCase();
+        char[] key = keyword.toCharArray();
 
-        System.out.println("Ciphertext : "
-                + cipher_text + "\n");
+        char[] encrypted = crypt(plain, key, 1);
+        System.out.println("Verschluesselter Text:");
+        System.out.println(encrypted);
+        createWriteFileEncrypt(String.valueOf(encrypted));
 
-        System.out.println("Original/Decrypted Text : "
-                + originalText(cipher_text, key));
+        char[] decrypted = crypt(encrypted, key, 0);
+        System.out.println("Entschluesselter Text:");
+        System.out.println(decrypted);
+        createWriteFileDecrypt(String.valueOf(decrypted));
+        scanner.close();
     }
-
 }
